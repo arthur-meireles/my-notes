fc649b023475062732e2fdd5eb465d3e0ce928f8
arthur-meireles <arthur_meireles@hotmail.com.br>
2020-08-03 11:27:51 -0300

    Inital Commit

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, SafeAreaView, StatusBar,TouchableOpacity} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor="#FDA905" barStyle="light-content"/>
+      
+      <View style={styles.content}>
+        <Text style={styles.title}>My Notes</Text>
+      </View>
+
+      {/* AQUI VAI A LISTAGEM */}
+      <TouchableOpacity>
+
+      </TouchableOpacity>
+
+    </SafeAreaView>
+  )
 }
 
+
+/*
+         STYLESHEET
+*/
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#FDA905',
   },
+  title:{
+    textAlign: 'center',
+    marginTop: 20,
+    color: 'white',
+    paddingBottom: 10,
+    fontSize: 30,
+    fontWeight: 'bold',
+
+  },
+  content:{
+
+  }
+
 });
+
